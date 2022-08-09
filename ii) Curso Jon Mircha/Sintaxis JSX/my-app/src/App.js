6b53bc229e6ee82007dea66d982aba99e4364507
Federico Irarrazaval <fedeirar@gmail.com>
2022-08-09 18:05:39 -0300

    video 2

--- ii) Curso Jon Mircha/Sintaxis JSX/my-app/src/App.js
+++ ii) Curso Jon Mircha/Sintaxis JSX/my-app/src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let nombre = "Fede",
+    auth;
+
+  let estaciones = ["primavera", "otoño"];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor="nombre"> Nombre</label>
+        <input type="text" id="nombre"></input>
+        <h2>{nombre}</h2>
+        <p>
+          {auth ? "El usuario no esta logueado" : "el usuario esta logueado"}
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          {estaciones.map((estacion, index) => (
+            <li key={index}>{estacion}</li>
+          ))}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
